8f8bc1f19e300b7672c59396b28db4fa84d4c364
David Mohl <david.mohl@hotmail.de>
2011-04-08 16:36:48 +0200

    Bugfix: Removed required Loop.php

--- php/parser.php
+++ php/parser.php
@@ -4,11 +4,13 @@
  * and open the template in the editor.
  */
 
+if ( !isset($POST['oosbl']) )
+    exit();
+
 require_once 'classes/StringRunner.php';
 require_once 'classes/Parser.php';
 require_once 'classes/Sprite.php';
 require_once 'classes/Effect.php';
-require_once 'functions/Loop.php';
 
 $oosbl = $_POST['oosbl'];
 $parser = new Parser();
